9f38a3e76ccc649b1d8b11b5df455aec519c7a5a
Shriram Jadhav <shrijadh@cisco.com>
2017-07-23 08:53:47 +0530

    Add functionality for computing and caching matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file provides two functions
+##  "makeCacheMatrix" : to cache the inverse of matrix
+##  "cacheSolve" : to lookup in cache, if not found calculate inverse of matrix
 
-## Write a short comment describing this function
+## Following function creates special matrix object and provides following functionality
+##  1. get matrix
+##  2. set matrix & internally unset the inverse of matrix when provided matrix does not match with old one
+##  3. setInverse of matrix
+##  4. getInverse of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseM <- NULL
+  set <- function (y) {
+    # Check if the matrix has not changed
+    #Ref : https://stat.ethz.ch/pipermail/r-help/2012-June/315408.html
+    if (!(is.matrix(x) &&
+          is.matrix(y) && dim(x) == dim(y) && all(x == y))) {
+      inverseM <- NULL
+    }
+    x <<- y
+  }
+  
+  get <- function () {
+    x
+  }
+  
+  setInverse <- function (inverse) {
+    inverseM <<- inverse
+  }
+  
+  getInverse <- function () {
+    inverseM
+  }
+  list(
+    set = set,
+    get = get,
+    setInverse = setInverse,
+    getInverse = getInverse
+  )
 }
 
 
-## Write a short comment describing this function
+## Following function checks whether inverse exits in cache otherwise ...
+##  calculates the inverse of the matrix provided by function "makeCacheMatrix"
+## Returns the inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverseM <- x$getInverse()
+  if (!is.null(inverseM)) {
+    message("getting cached data")
+    return(inverseM)
+  }
+  data <- x$get()
+  inverseM <- solve(data, ...)
+  x$setInverse(inverseM)
+  inverseM
 }
